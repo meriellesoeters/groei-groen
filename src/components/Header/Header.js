f678eaf6120d2c48f182ced2c8879ed2c1afcfde
Merielle Soeters <meriellesoeters@gmail.com>
2017-07-23 12:36:33 +0200

    change # in something more usefull for links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,11 +5,11 @@ const Header = () => (
   <div className="HeaderContainer">
       <div className="HeaderItemContainer">
           <div className="HeaderItems HeaderItems--left">
-              <a href="#" className="HeaderButton">Groei</a>
-              <a href="#" className="HeaderButton">Eet</a>
+              <a href="/groei" className="HeaderButton">Groei</a>
+              <a href="/eet" className="HeaderButton">Eet</a>
           </div>
           <div className="HeaderItems HeaderItems--right">
-              <a href="#" className="HeaderButton">Log in</a>
+              <a href="/login" className="HeaderButton">Log in</a>
           </div>
       </div>
   </div>
